845c58683e7973b03622902a93af8038b9c105cb
1157458634@qq.com <1157458634@qq.com>
2019-10-24 15:41:47 +0800

    版本控制

---
+++ api-gateway/src/main/java/com/cookie/ApiGatewayApplication.java
@@ -0,0 +1,18 @@
+package com.cookie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@SpringBootApplication
+@EnableFeignClients
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+
+}
