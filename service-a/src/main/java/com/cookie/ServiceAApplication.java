4de1d3f0113fccefca7ee3fa25700892584f9954
1157458634@qq.com <1157458634@qq.com>
2019-10-28 17:21:32 +0800

    使用灰度发布时，不需要该注解

--- service-a/src/main/java/com/cookie/ServiceAApplication.java
+++ service-a/src/main/java/com/cookie/ServiceAApplication.java
@@ -13,7 +13,6 @@ import org.springframework.context.annotation.FilterType;
 
 @EnableFeignClients
 @SpringCloudApplication
-@ComponentScan("com.cookie")
 public class ServiceAApplication {
 
 	public static void main(String[] args) {
